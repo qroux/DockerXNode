ea3c9a3077fc84c7597c8bda5c1b20543e8c147d
quentin roux quentin.roux@hotmail.fr <quentin.roux@hotmail.fr>
2019-08-30 14:42:53 +0200

    Dockerfile setup with npm + local files copy

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+# specify a base image
+FROM node:alpine
+
+# Install some dependencies
+COPY ./ ./
+RUN npm install
+
+# Default command
+CMD ["npm", "start"]
--- index.js
+++ index.js
@@ -6,5 +6,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(8080, () => {
-  console.log('Listnening on port 8080');
+  console.log('Listening on port 8080');
 });
